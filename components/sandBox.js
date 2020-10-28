2c63f15359798bcf9e013244503e66968ac1efb0
Emdadul Islam <68099504+emdadul-islam439@users.noreply.github.com>
2020-10-28 20:58:17 +0600

    uploaded all the components

---
+++ components/sandBox.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function SandBox(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>One</Text>
+            <Text style={styles.boxTwo}>Two</Text>
+            <Text style={styles.boxThree}>Three</Text>
+            <Text style={styles.boxFour}>Four</Text>
+        </View>
+    ); 
+}
+const styles=StyleSheet.create({
+    container: {
+        // flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'flex-end',
+        paddingTop: 40,
+        backgroundColor: '#ddd',
+    },
+    boxOne:{
+        flex: 1,
+        backgroundColor: 'violet',
+        padding: 10,
+    },
+    boxTwo:{
+        flex: 3,
+        backgroundColor: 'gold',
+        padding: 20,
+    },
+    boxThree:{
+        flex:1,
+        backgroundColor: 'coral',
+        padding: 30,
+    },
+    boxFour:{
+        flex: 5,
+        backgroundColor: 'skyblue',
+        padding: 40,
+    },
+})
